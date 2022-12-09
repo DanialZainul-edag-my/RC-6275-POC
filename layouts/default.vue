15912a5280830eba0f76c2c4986edff5af661521
Danial Zainul <muhammad.danial-zainul@edag.com.my>
2022-12-09 11:59:05 +0800

    Add nav drawer, car ecu and data pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,49 @@
 <script setup>
 import { ref } from 'vue'
+import { useI18n } from 'vue-i18n';
 
 const theme = ref('light')
+const { t } = useI18n({ useScope: 'global' })
+const drawer = ref(true)
+const drawerItems = ref(
+  [
+    {
+      'title': t('dashboard'),
+      'icon': 'mdi-view-dashboard',
+      'url': '/'
+    },
+    {
+      'title': t('car.carECU'),
+      'icon': 'mdi-car',
+      'url': '/car-ecu'
+    },
+    {
+      'title': t('car.carData'),
+      'icon': 'mdi-chart-line-variant',
+      'url': '/car-data'
+    }
+  ]
+)
 
 const switchTheme = () => {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+const toggleDrawer = () => {
+  drawer.value = !drawer.value
 }
 </script>
 
 <template>
   <v-app :theme="theme">
-    <v-app-bar :title="$t('application.name')" color="primary">
+    <v-app-bar
+      :title="$t('application.name')"
+      color="primary"
+      @click="$router.push('/')"
+    >
+      <template #prepend>
+        <v-app-bar-nav-icon @click.stop="toggleDrawer"></v-app-bar-nav-icon>
+      </template>
+
       <div class="d-flex flex-row mx-2">
         <v-btn
           class="mx-2"
@@ -21,7 +54,21 @@ const switchTheme = () => {
       </div>
     </v-app-bar>
 
-    <!-- <v-navigation-drawer></v-navigation-drawer> -->
+    <v-navigation-drawer v-model="drawer">
+      <v-list
+        v-for="item in drawerItems"
+        :key="item.title"
+        color="transparent"
+      >
+        <NuxtLink :to="item.url">
+          <v-list-item
+            :prepend-icon="item.icon"
+            :title="item.title"
+          >
+          </v-list-item>
+        </NuxtLink>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container>
---
+++ pages/car-data.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<h1>{{ $t('car.carData') }}</h1>
+	</div>
+</template>
