43837129bd79424d5ec17c4d84d65bf20938cea5
Danial Zainul <muhammad.danial-zainul@edag.com.my>
2022-12-16 16:46:49 +0800

    Add global css file

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,8 @@ import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 export default defineNuxtConfig({
   css: [
     'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css'
+    '@mdi/font/css/materialdesignicons.min.css',
+    '@/assets/css/main.scss'
   ],
   build: {
     transpile: ['vuetify']
